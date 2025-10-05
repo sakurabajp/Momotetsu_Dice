7058d884cd372b21ce48d982ac0a97faab554bb2
sakurabajp <sakuraba8050@gmail.com>
2025-10-05 14:36:41 +0900

    カードを使用した時にメッセージを表示するように修正

--- src/main/java/mc/cherry_leaves/net/momotetsu2/CardSystem.java
+++ src/main/java/mc/cherry_leaves/net/momotetsu2/CardSystem.java
@@ -1,14 +1,33 @@
 package mc.cherry_leaves.net.momotetsu2;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
+
+import java.util.Objects;
+
+import static mc.cherry_leaves.net.momotetsu2.card.CardList;
 
 public class CardSystem implements Listener {
 
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent e) {
         Item item = e.getItemDrop();
+        if (CardList.contains(item.getItemStack())) {
+            e.getItemDrop().remove();
+            for(Player p : Bukkit.getOnlinePlayers()) {
+                p.sendMessage(Component.text(e.getPlayer().getName() + "が")
+                        .append(Component.text(" " + item.getItemStack().getItemMeta().displayName() + " ").color(NamedTextColor.YELLOW))
+                        .append(Component.text("を使用しました！")).color(NamedTextColor.WHITE));
+                p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 0.8f, 0.1f);
+            }
+        }
     }
 }
